4036d31af9ed3bda58da084bee16ad5d40479301
deev6062 <104971487+deev6062@users.noreply.github.com>
2022-07-29 21:48:34 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,15 @@
+let movies = [
+    {
+        image: 'badging4.jpg'
+    },
+    {
+        image: 'badging5.jpg'
+    },
+    {
+        image: 'scale16.jpg'
+    },
+    {
+        image: 'scale6.jpg'
+    }
+
+]
